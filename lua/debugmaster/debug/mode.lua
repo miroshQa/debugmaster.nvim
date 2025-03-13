c9464cfd5d9b34374d161f748180dd1345771710
Mirion <mironnesvetaylov@gmail.com>
2025-03-13 16:21:56 +0300

    trying to bind cursorline_hl only to current window

--- lua/debugmaster/debug/mode.lua
+++ lua/debugmaster/debug/mode.lua
@@ -18,6 +18,7 @@ local guicursor_orig = vim.opt.guicursor._value
 local cursorline_orig = vim.o.cursorline
 local cursorline_bg_orig = vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID("CursorLine")), "bg")
 -- TODO: I guess CursorLineNr also would be cool
+local cursorline_au_id = nil
 
 local function save_original_settings()
   local all = vim.api.nvim_get_keymap("n")
@@ -43,6 +44,7 @@ local function save_original_settings()
 end
 save_original_settings()
 
+local last_entered_win = 0
 function M.activate()
   if active then
     return
@@ -55,13 +57,37 @@ function M.activate()
     end
   end
 
-  vim.go.cursorline = true
+  -- we want to see cursor line only in the current split
+  -- TODO: Open PR in neovim repo. Because this basic feature should be in the core
+  -- Add new hl group CursorLineInactive
+  cursorline_au_id = vim.api.nvim_create_autocmd("WinEnter", {
+    callback = function(args)
+      if vim.api.nvim_win_is_valid(last_entered_win) then
+        -- see :help vim.wo
+        vim.wo[last_entered_win].cursorline = false
+      end
+      last_entered_win = vim.api.nvim_get_current_win()
+      vim.wo.cursorline = true
+    end
+  })
+
+  -- NOTES: Trying to undestand all those b, bo,g, go, opt, o, wo, ...
+  --- Basically we have two entities: variables, options
+  --- Options has "o" at the end.
+  --- So vim.b - is buffer scoped variables, vim.g - global variables
+  --- vim.bo - is buffer scoped OPTIONS, vim.go - global options
+  --- what the hell is vim.opt then?
+  last_entered_win = vim.api.nvim_get_current_win()
+  vim.wo.cursorline = true
   vim.api.nvim_set_hl(0, "CursorLine", {bg = "#2c4e28"})
   vim.api.nvim_set_hl(0, "dCursor", { bg = "#2da84f" })
   vim.go.guicursor = "n-v-sm:block-dCursor,i-t-ci-ve-c:ver25,r-cr-o:hor20"
 end
 
 function M.disable()
+  if not active then
+    return
+  end
   active = false
   for _, group in ipairs(groups) do
     for _, mapping in ipairs(group.mappings) do
@@ -74,9 +100,12 @@ function M.disable()
       })
     end
   end
-  vim.go.cursorline = cursorline_orig
+  vim.wo[0].cursorline = cursorline_orig
   vim.api.nvim_set_hl(0, "CursorLine", {bg = cursorline_bg_orig})
   vim.go.guicursor = guicursor_orig
+  if cursorline_au_id then
+    vim.api.nvim_del_autocmd(cursorline_au_id)
+  end
 end
 
 function M.toggle()
