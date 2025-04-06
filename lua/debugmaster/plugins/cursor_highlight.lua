4a9ec6f812cf3e5ea076ff25e68620f56c44673c
Mirion <mironnesvetaylov@gmail.com>
2025-04-06 20:42:26 +0300

    Don't highlight cursor in the terminal when debug mode enabled
    Useful when you using neovim inside neovim loool

--- lua/debugmaster/plugins/cursor_highlight.lua
+++ lua/debugmaster/plugins/cursor_highlight.lua
@@ -1,7 +1,7 @@
 local activate_cursor_hl = function()
   vim.api.nvim_set_hl(0, "dCursor", { bg = "#2da84f" })
   local cursor_mode_off = "n-v-sm:block,i-t-ci-ve-c:ver25,r-cr-o:hor20"
-  local cursor_mode_on = "n-v-sm:block-dCursor,i-t-ci-ve-c:ver25-dCursor,r-cr-o:hor20"
+  local cursor_mode_on = "n-v-sm:block-dCursor,i-ci-ve-c:ver25-dCursor,t:ver25,r-cr-o:hor20"
 
   vim.api.nvim_create_autocmd("User", {
     pattern = "DebugModeChanged",
