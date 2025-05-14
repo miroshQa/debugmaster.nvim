fe59697614742ec2e5afbbbfe8e556eb120a18ad
Mirion <mironnesvetaylov@gmail.com>
2025-05-14 08:41:54 +0300

    refactor: kill previous tree implementation with fire

--- lua/debugmaster/ui/Breakpoints.lua
+++ lua/debugmaster/ui/Breakpoints.lua
@@ -1,133 +1,130 @@
 local dap = require("dap")
 local breakpoints = require("dap.breakpoints")
-local Tree = require("debugmaster.ui.Tree")
+local tree = require("debugmaster.ui.tree")
 local api = vim.api
 
----@class dm.BreakpointNode: dm.NodeTrait
-local BreakpointNode = {}
 
----@param bpoints {buf: number, bpoints: any}?
-function BreakpointNode.new(bpoints, bpoint)
-  ---@class dm.BreakpointNode
-  local self = setmetatable({}, { __index = BreakpointNode })
-  self.id = "first"
-  self.bpoints = bpoints
-  self.bpoint = bpoint
-  if bpoints or bpoint then
-    self.id = bpoints and ("buf" .. bpoints.buf) or ("bp" .. bpoint.line)
-  end
-  return self
-end
-
-function BreakpointNode:get_children_iter()
-  if not self.bpoint and not self.bpoints then
-    return coroutine.wrap(function()
-      for buf, bpoints in pairs(breakpoints.get()) do
-        coroutine.yield(BreakpointNode.new({ buf = buf, bpoints = bpoints }))
-      end
-    end)
-  elseif self.bpoints then
-    return vim.iter(self.bpoints.bpoints):map(function(b)
-      b.buf = self.bpoints.buf
-      return BreakpointNode.new(nil, b)
-    end)
-  end
-  return function() end
-end
-
-function BreakpointNode:get_repr()
-  if not self.bpoints and not self.bpoint then
-    local help = {
-      { { "t - remove breakpoint or all breakpoints in the file", "Comment" } },
-      { { "c - change breakpoint condition", "Comment" } }
-    }
-    return { { "Breakpoints", "Exception" } }, help
-  elseif self.bpoints then
-    local path = api.nvim_buf_get_name(self.bpoints.buf)
-    path = vim.fn.fnamemodify(path, ":.")
-    return { { path, "Statement" } }
-  else
-    local vlines = nil
-    local indent = "    "
-    local linenr = self.bpoint.line
-    local line = vim.trim(api.nvim_buf_get_lines(self.bpoint.buf, linenr - 1, linenr, false)[1])
-    local text = string.format("%s %s %s", indent, linenr, line)
-    local condition = self.bpoint.condition
-    if condition and condition ~= "" then
-      vlines = { { { indent }, { "condition: ", "Comment" }, { self.bpoint.condition } } }
-    end
-    return { { text } }, vlines
-  end
-end
-
-function BreakpointNode:is_expanded()
-  return true
-end
 
 ---@class dm.ui.Breakpoints: dm.ui.Sidepanel.IComponent
 local Breakpoints = {}
 
 function Breakpoints.new()
-  ---@class dm.ui.Breakpoints
-  local self = setmetatable({}, { __index = Breakpoints })
+  local self = {}
   self.name = "[B]points"
-  self._tree = Tree.new_with_buf(BreakpointNode.new())
-  self.buf = self._tree.buf
+  self.buf = api.nvim_create_buf(false, true)
+  -- TODO: figure out if lua_ls handle {kind = "dir", ...} or {kind = "file", ...} notation right.
+  -- That would be blast. We can immitate rust enums then. (How in typescript)
+  -- https://github.com/jrop/u.nvim/blob/v2/examples/filetree.lua
+  -- But probably it doesn't work. Because lua_ls is a dog shit to be honest (compared to ts_ls)
+
+  ---@alias dm.BpContainer {kind: "container", children: dm.Bp[], buf: number, expanded: boolean }
+  ---@alias dm.Bp {kind: "bp", line: number, condition?: string, buf: number}
+  ---@alias dm.BpDummyRoot {kind: "dummyRoot"}
+  ---@alias dm.BpTreeNode dm.BpContainer | dm.Bp
+
+  ---@param node dm.BpTreeNode
+  local function render_node(node)
+    -- yeah {kind = "a"} or {kind = "b"} notation doesn't work indeed
+    -- Please someone create good lsp server for lua please...
+    if node.kind == "dummyRoot" then
+      local help = {
+        { { "t - remove breakpoint or all breakpoints in the file", "Comment" } },
+        { { "c - change breakpoint condition", "Comment" } }
+      }
+      return { { "Breakpoints", "Exception" } }, { vlines = help }
+    elseif node.kind == "container" then
+      local path = api.nvim_buf_get_name(node.buf)
+      path = vim.fn.fnamemodify(path, ":.")
+      return { { path, "Statement" } }
+    else
+      local vlines = nil
+      local indent = "    "
+      local linenr = node.line
+      local line = vim.trim(api.nvim_buf_get_lines(node.buf, linenr - 1, linenr, false)[1])
+      local text = string.format("%s %s %s", indent, linenr, line)
+      local condition = node.condition
+      if condition and condition ~= "" then
+        vlines = { { { indent }, { "condition: ", "Comment" }, { node.condition } } }
+      end
+      return { { text } }, vlines
+    end
+  end
+
+  local snapshot
+  local function rerender_tree()
+    ---@type dm.BpDummyRoot
+    local root = {
+      kind = "dummyRoot",
+      expanded = true,
+      children = {},
+    }
+    for buf, bpoints in pairs(breakpoints.get()) do
+      ---@type dm.Bp[]
+      local children = {}
+      for _, bp in ipairs(bpoints) do
+        bp.buf = buf
+        bp.kind = "bp"
+        table.insert(children, bp)
+      end
+      ---@type dm.BpContainer
+      local container = { children = children, expanded = true, buf = buf, kind = "container" }
+      table.insert(root.children, container)
+    end
+    snapshot = tree.render { buf = self.buf, root = root, renderer = render_node }
+  end
+  rerender_tree()
 
   vim.keymap.set("n", "c", function()
     local line = api.nvim_win_get_cursor(0)[1] - 1
-    ---@type dm.BreakpointNode?
-    local node = self._tree:node_by_line(line)
-    if node and node.bpoint then
-      local bp = node.bpoint
-      local condition = vim.fn.input({ default = bp.condition or "" })
-      breakpoints.set({ condition = condition }, bp.buf, bp.line)
-      self._tree:render()
+    ---@type dm.BpTreeNode
+    local node = snapshot.nodes_by_line[line]
+    if node.kind == "bp" then
+      local condition = vim.fn.input({ default = node.condition or "" })
+      breakpoints.set({ condition = condition }, node.buf, node.line)
+      rerender_tree()
     end
   end, { buffer = self.buf, nowait = true })
 
 
   vim.keymap.set("n", "t", function()
     local line = api.nvim_win_get_cursor(0)[1] - 1
-    ---@type dm.BreakpointNode?
-    local node = self._tree:node_by_line(line)
-    if node then
-      if node.bpoint then
-        local bp = node.bpoint
-        breakpoints.remove(bp.buf, bp.line)
+    ---@type dm.BpTreeNode
+    local node = snapshot.nodes_by_line[line]
+    if not node then
+      return
+    end
+    if node.kind == "bp" then
+      breakpoints.remove(node.buf, node.line)
+      for _, session in pairs(dap.sessions()) do
+        session:set_breakpoints(breakpoints.get(node.buf))
+      end
+    elseif node.kind == "container" then
+      for _, bp in pairs(node.children) do
+        breakpoints.remove(node.buf, bp.line)
         for _, session in pairs(dap.sessions()) do
           session:set_breakpoints(breakpoints.get(bp.buf))
         end
-      elseif node.bpoints then
-        local bps = node.bpoints or {}
-        for _, bp in pairs(bps.bpoints) do
-          breakpoints.remove(bps.buf, bp.line)
-          for _, session in pairs(dap.sessions()) do
-            session:set_breakpoints(breakpoints.get(bp.buf))
-          end
-        end
       end
-      self._tree:render()
     end
+    rerender_tree()
   end, { buffer = self.buf, nowait = true })
 
   vim.keymap.set("n", "<CR>", function()
     local line = api.nvim_win_get_cursor(0)[1] - 1
-    ---@type dm.BreakpointNode?
-    local node = self._tree:node_by_line(line)
+    ---@type dm.BpTreeNode
+    local node = snapshot.nodes_by_line[line]
     if node then
-      if node.bpoint then
-        local bp = node.bpoint
-        breakpoints.remove(bp.buf, bp.line)
+      if node.kind == "bp" then
+        breakpoints.remove(node.buf, node.line)
         vim.cmd("q")
-        vim.cmd("buffer " .. bp.buf)
-        vim.cmd("normal " .. bp.line .. "G")
+        vim.cmd("buffer " .. node.buf)
+        vim.cmd("normal " .. node.line .. "G")
       end
     end
   end)
 
   dap.listeners.after.setBreakpoints["debugmaster"] = function()
-    self._tree:render()
+    rerender_tree()
   end
 
   return self
